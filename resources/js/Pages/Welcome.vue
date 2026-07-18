980f5e3276f0f398ff79cd5a6b50ff292f875214
mahdy-hasan-9 <mhdyhasan017@gmail.com>
2026-07-18 12:03:47 +0600

    configured-a-welcome-page-with-inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    message: String
+});
+</script>
+
+
+<template>
+    <div class="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 class="text-2xl font-bold text-blue-600">{{ message }}</h1>
+    </div>
+</template>
